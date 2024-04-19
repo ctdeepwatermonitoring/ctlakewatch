c4273a043d8ce88020685840e5e7680362cac57f
Chloe Edwards <chloe.edwards@ct.gov>
2024-04-19 15:30:56 -0400

    update reportloop.R

--- reportloop.R
+++ reportloop.R
@@ -1,32 +1,27 @@
-#script for generating lake watch reports on a loop
-setwd("P:/R/ctlakewatch/reports")
+# script for generating lake watch reports on a loop
+# setwd("P:/R/ctlakewatch/reports")
 
 library(dplyr)
 library(rmarkdown)
 
-stations <- read.csv("stations.csv")
+stations <- read.csv("stations.csv") # manually tweaking stations file due to data sources
 siteIDs <- unique(stations$MonitoringLocationName) # my loop variables
 
 for (i in siteIDs) {
   # filter stations to current iteration
   station <- filter(stations, MonitoringLocationName == i)
-  
   # get comID and stationID for loop params
   comID <- unique(station$ComID) # unique means it wont break for multiple stations per waterbody
   stationID <- unique(station$MonitoringLocationName)
-  
   # base file to modify
   base <- readLines("lakewatchReportv2.Rmd")
-  
   # replace the parameter
   base <- gsub("SiteID: .*", paste("SiteID:", stationID), base)
-  
   # write files to reports subfolder
   outputFile <- file.path("P:/R/ctlakewatch/reports", paste0(comID, ".Rmd"))
   writeLines(base, outputFile)
-  
   # render rmd file
   render(input = outputFile, 
          output_file = file.path("P:/R/ctlakewatch/reports", paste0(comID, ".html")),
-         output_format = "html_document")  # Adjust output_format as needed
+         output_format = "html_document")  
 }
