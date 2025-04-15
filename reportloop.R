0048a5a002e6cb3a4e20fa0680976a1c7cf6b20e
Mary Becker <mary.becker@ct.gov>
2025-04-15 14:12:03 -0400

    update rpts with correct lake depths

--- reportloop.R
+++ reportloop.R
@@ -13,10 +13,13 @@ for (i in siteIDs) {
   # get comID and stationID for loop params
   comID <- unique(station$ComID) # unique means it wont break for multiple stations per waterbody
   stationID <- unique(station$MonitoringLocationName)
+  maxDepth <- unique(station$MaxDepth)
   # base file to modify
-  base <- readLines("lakewatchReportv2.Rmd")
-  # replace the parameter
+  base <- readLines("C:/Users/deepuser/Documents/Projects/ProgramDev/ctlakewatch/lakewatchReportv2.Rmd")
+  # replace the station
   base <- gsub("SiteID: .*", paste("SiteID:", stationID), base)
+  # replace the depth
+  base <- gsub("MaxDepth: .*", paste("MaxDepth:", maxDepth), base)
   # write files to reports subfolder
   lfPath <- "C:/Users/deepuser/Documents/Projects/ProgramDev/ctlakewatch/"
   outputFile <- file.path(lfPath, paste0(comID, ".Rmd"))
@@ -26,3 +29,4 @@ for (i in siteIDs) {
          output_file = file.path(paste0(lfPath,"reports"), paste0(comID, ".html")),
          output_format = "html_document")  
 }
+
